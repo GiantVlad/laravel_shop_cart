304ee30cd1352ae1e719b5da46a674430edb3abb
GaintVlad <sadkoff@tut.by>
2017-12-17 22:39:46 +0300

    fix app service and search controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,13 +16,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*$catalogs = Catalog::where('parent_id', NULL)->get();
+        View::share('catalogs', Catalog::where('parent_id', NULL)->get());
 
-        View::share('catalogs', $catalogs);*/
-
-        //Delete record from "product_property" table
+        //Delete records from "product_property" table
         Product::deleting(function ($product) {
-            $product->properties()->sync([]);
+            $product->properties()->detach();
         });
     }
 
--- resources/views/shop/search-product.blade.php
+++ resources/views/shop/search-product.blade.php
@@ -4,7 +4,7 @@
 
             <div class="col-md-6 col-sm-12">
                 <h5>{{$product->name}}</h5>
-                <a href="shop/{{$product->id}}"><img class="img-thumbnail" alt="product id {{ $product->id }}"
+                <a href="{{ url('/shop/'.$product->id) }}"><img class="img-thumbnail" alt="product id {{ $product->id }}"
                                                      width="300" height="200"
                                                      src="{{ asset('images/'.$product->image) }}">
                 </a>
