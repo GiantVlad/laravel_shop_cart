0a61e9580e719a32840ed767ac255125b5e4c79c
Uladzimir Sadkou <uladzimir.sadkou@paxful.com>
2023-08-15 20:30:38 +0300

    Update dependencies
    - added https support
    - added compose.yaml to deploy containers to MS Azure ACI.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Temporal\Client\GRPC\ServiceClient;
 use Temporal\Client\WorkflowClient;
@@ -16,6 +17,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot ()
     {
+        if(env('APP_HTTPS_FOR_STATIC', false))
+        {
+            $this->app->get('request')->server->set('HTTPS','on');
+            URL::forceScheme('https');
+        }
 //        if (!$this->app->runningInConsole()) {
 //            $catalog = new Catalog;
 //            View::share('catalogs', $catalog->parentsNode());
